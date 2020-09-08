c20fb7a6d654281c851e45cee52490f15252fbed
vndee <hvd.huynhduy@gmail.com>
2020-09-08 10:04:11 +0700

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
+pip3 install -r requirements.txt
+
 # Install transformers port by PhoBERT (VinAI)
 git clone https://github.com/datquocnguyen/transformers.git
 cd transformers
 pip3 install .
 
 # Install vncorenlp for word segmenter
+cd ..
 pip3 install vncorenlp
 mkdir -p vncorenlp/models/wordsegmenter
 wget https://raw.githubusercontent.com/vncorenlp/VnCoreNLP/master/VnCoreNLP-1.1.1.jar
