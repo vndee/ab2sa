198d26f45486301ff9faed89ea6b487784a4fd34
vndee <hvd.huynhduy@gmail.com>
2020-09-14 17:09:58 +0700

    update loss

--- phobert_clf_topic.py
+++ phobert_clf_topic.py
@@ -35,7 +35,7 @@ class PhobertABSA(nn.Module):
 if __name__ == '__main__':
     num_epochs = 10
     device = 'cuda'
-    batch_size = 2
+    batch_size = 16
     accumulation_step = 50
 
     train, test = VLSP2018BertPairTopic(data='Hotel', file='train'), VLSP2018BertPairTopic(data='Hotel', file='test')
